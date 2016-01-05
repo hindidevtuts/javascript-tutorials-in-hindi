848b4db5ee8b2caaa25cc21ba434ab330cf14b4d
hindidevtuts <hindidevtuts@gmail.com>
2016-01-05 22:56:53 +0530

    all about variables

---
+++ day_04/script.js
@@ -0,0 +1,18 @@
+// var age = 26 ;//number
+// var name = "edword";//string
+// alert(age);
+// alert(name);
+
+var num1= 4;
+var num2= 5;
+var results=num1+num2;
+alert(results);
+
+// The general rules for constructing names for variables (unique identifiers) are:
+
+//     Names can contain letters, digits, underscores, and dollar signs.
+//     Names must begin with a letter
+//     Names can also begin with $ and _ (but we will not use it in this tutorial)
+//     Names are case sensitive (y and Y are different variables)
+//     Reserved words (like JavaScript keywords) cannot be used as names
+
